1a50438b7d3eb42aa8e5f325c7afc03914967bb6
Myah <myahtyga64@gmail.com>
2026-06-17 23:23:08 +0100

    keep user logged in

--- src/services/firebase.js
+++ src/services/firebase.js
@@ -1,7 +1,7 @@
 import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
-
+import { setPersistence, browserLocalPersistence } from "firebase/auth";
 const firebaseConfig = {
   apiKey: "AIzaSyCjLVUF_wyjlduSuCi_4DEyKXOt8JjEvsQ",
   authDomain: "coachpulse-87341.firebaseapp.com",
@@ -14,4 +14,5 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 
 export const auth = getAuth(app);
+setPersistence(auth, browserLocalPersistence);
 export const db = getFirestore(app);
